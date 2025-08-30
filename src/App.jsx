aaf1bc45f186eb50cf6020ad46e93d53b31a136c
Muhammed-Anees-P <aneespengad4447@gmail.com>
2025-08-30 14:06:33 +0530

    feat(app): implement authentication and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+// App.js
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import { ROUTES } from "./routes";
 
-const App = () => {
+
+function App() {
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  
+  const handleLogin = (token, expiresAt) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+    localStorage.setItem("expiresAt", expiresAt);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiresAt");
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+      <Routes>
+        <Route 
+          path="/" 
+          element={token ? <Navigate to={ROUTES.HOME} /> : <Navigate to={ROUTES.LOGIN} />} 
+        />
+        <Route 
+          path={ROUTES.LOGIN} 
+          element={!token ? <Login onLogin={handleLogin} /> : <Navigate to={ROUTES.HOME} />} 
+        />
+        <Route 
+          path={ROUTES.HOME} 
+          element={token ? <Home onLogout={handleLogout} /> : <Navigate to={ROUTES.LOGIN} />} 
+        />
+      </Routes>
+  );
 }
 
-export default App
+export default App;
